193faadc26176f31b8671f849c951d581894541b
Dao Minh Chau <chau.dao-minh@internal.banvien.com>
2024-05-02 16:46:54 +0700

    hahaha tailwind

--- src/components/beaker/Beaker.tsx
+++ src/components/beaker/Beaker.tsx
@@ -1,5 +1,4 @@
-import React, { useMemo, useState } from "react";
-import "./Beaker.css";
+import React, { useState } from "react";
 
 interface BeakerProps {}
 interface Item {
@@ -94,28 +93,26 @@ const Beaker: React.FC<BeakerProps> = () => {
   const handleRenderItem = (title: string) => {
     if (parseInt(title) === 0) {
       return (
-        <span style={{ fontSize: "10.5px", fontWeight: "bold" }}>
-          <span style={{ fontWeight: "bold" }}>- </span>
+        <span className=" text-[10.5px] font-bold">
+          <span className="font-bold">- </span>
           {title}
         </span>
       );
     }
     if (!parseInt(title)) {
-      return (
-        <span style={{ fontSize: "10.5px", fontWeight: "bold" }}> {title}</span>
-      );
+      return <span className=" text-[10.5px] font-bold"> {title}</span>;
     } else {
       if (parseInt(title) === max) {
         return (
-          <span style={{ fontSize: "10.5px", fontWeight: "bold" }}>
-            <span style={{ fontWeight: "bold" }}>- </span>
+          <span className=" text-[10.5px] font-bold">
+            <span className="font-bold">- </span>
             {title} mL
           </span>
         );
       } else {
         return (
-          <span style={{ fontSize: "10.5px", fontWeight: "bold" }}>
-            <span style={{ fontWeight: "bold" }}>- </span>
+          <span className=" text-[10.5px] font-bold">
+            <span className="font-bold">- </span>
             {title}
           </span>
         );
@@ -124,37 +121,29 @@ const Beaker: React.FC<BeakerProps> = () => {
   };
 
   return (
-    <div className="container">
-      <div className="top">
-        <div className="circle">
-          <div className="in"></div>
+    <div className="container relative top-[50px] w-[210px]">
+      <div className="top absolute top-[-30px] left-0 bg-gradient-to-r from-[#d0e9f9] to-[#fff] w-full h-[60px] rounded-[50%] border-[1px] border-solid border-[#bdb3b3] z-[100] ">
+        <div className="circle absolute top-[5px] left-[10px]  h-[50px] bg-gradient-to-l from-[#d0e9f9] to-[#fff] rounded-[50%] border-[1px] border-solid border-[#bdb3b3] box-border w-[calc(100%-20px)]">
+          <div className="in absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#d0e9f9] to-[#fff] rounded-[50%]"></div>
         </div>
       </div>
-      <div className="cup">
-        <div className="wrap-water">
+      <div className="cup relative w-[210px] h-[300px] bg-gradient-to-r from-[#d0e9f9] to-[#fff] rounded-b-[50px] border-[1px] border-solid border-[#bdb3b3] overflow-hidden">
+        <div className="wrap-water relative top-[97px] ">
           <div
-            className="water"
+            className="water absolute bottom-[-201px] w-[209px] bg-gradient-to-r from-[#c5f1c6] to-[#fff] rounded-b-[50px] z-0 border-[1px] border-solid border-[#bdb3b3]"
             style={{ height: (water - min) * (heightWater / max) + 50 - 12.5 }}
           >
-            <div className="water-top">
-              <div className="water-cricle">
-                <div className="water-in"></div>
+            <div className="water-top absolute top-[-30px] left-0 bg-gradient-to-r from-[#d0e9f9] to-[#fff] w-full h-[62px] rounded-[50%] border-[1px] border-solid border-[#bdb3b3]">
+              <div className="water-cricle absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#d0e9f9] to-[#fff] rounded-[50%] border-[1px] border-solid border-[#bdb3b3]">
+                <div className="water-in absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[#d0e9f9] to-[#fff] rounded-[50%]"></div>
               </div>
             </div>
           </div>
         </div>
         <div
-          className="ruler"
+          className="ruler flex flex-col-reverse w-fit absolute bottom-[20px] gap-[0.5px] left-[50%] z-[110]"
           style={{
-            display: "flex",
-            flexDirection: "column-reverse",
-            width: "fit-content",
-            position: "absolute",
             height: heightWater,
-            bottom: 20,
-            gap: 0.5,
-            left: "50%",
-            zIndex: 110,
           }}
         >
           {handleItem(max, min, step, smallStep).map((item) => {
@@ -177,7 +166,7 @@ const Beaker: React.FC<BeakerProps> = () => {
         step={smallStep}
         value={water}
         onChange={handleTemperatureChange}
-        className="progress"
+        className="progress w-[200px] mb-[20px] rotate-[-90deg] cursor-pointer absolute top-[50%] left-[150px]"
       />
     </div>
   );
